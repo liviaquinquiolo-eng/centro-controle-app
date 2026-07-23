1cecce7e214a7fefaebad106e809b81dc53f9e09
Livia Quinquiolo <livia.quinquiolo@docusign.com>
2026-07-23 15:41:54 -0300

    Add tickets route to server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+app.use('/api/tickets', require('./routes/tickets'));const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
